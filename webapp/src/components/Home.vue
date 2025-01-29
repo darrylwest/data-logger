8840bbacd10086334ea06299178d4bb1f2864410
darryl west <darryl.west@raincitysoftware.com>
2025-01-29 07:53:04 -0800

    separate data from chart

--- webapp/src/components/Home.vue
+++ webapp/src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto pt-10">
     <h2 class="text-3xl font-bold text-center">Temperature Readings</h2>
-    <h3 class="font-bold text-center ">January 28th, 2025 - 4am to 4pm</h3>
+    <h3 class="font-bold text-center ">2025-01-28 04:00:00 to 2025-01-28 16:00:00</h3>
     <div class="mt-4 mb-20">
       <canvas id="lineChart"></canvas>
     </div>
@@ -15,25 +15,45 @@ import { Chart, registerables } from 'chart.js';
 Chart.register(...registerables);
 
 // TODO : move this to an external file; passing start/stop datetime and interval (10min, 30min, 1 hour, etc.)
-function fetchReadings() {
-  const temps = [
-    {
-      label: 'Cottage-South',
-      data: [42.5, 42.9, 43.2, 42.2, 41.4, 40.8, 40.3, 40.4, 40.4, 43.2, 43.9, 45.3, 50.2, 52.3, 54.1, 55.3, 56.4, 58.2, 59.8, 60.2, 60.9, 59.3, 57.8, 56.9, 56.1],
-      fill: false,
-      borderColor: 'rgba(75, 192, 192, 1)',
-      tension: 0.1,
-    },
-    {
-      label: 'Shed-west',
-      data: [43.5, 43.9, 44.2, 43.2, 42.4, 41.8, 41.3, 41.4, 41.4, 44.2, 44.9, 46.3, 51.2, 52.3, 53.1, 54.3, 55.4, 57.2, 58.8, 61.2, 61.9, 60.3, 57.8, 56.3, 55.4],
-      fill: false,
-      borderColor: 'rgba(192, 75, 75, 1)',
-      tension: 0.1,
-    },
-  ]
+const TemperatureData = {
+  dataParams: () => {
+    const params = {
+      start_date: '2025-01-28 04:00:00',
+      end_date: '2025-01-28 16:00:00',
+      interval: '30',
+    }
 
-  return temps;
+    return params
+  },
+
+  fetchReadings: () => {
+    const temps = [
+      {
+        label: 'Cottage-South',
+        data: [42.5, 42.9, 43.2, 42.2, 41.4, 40.8, 40.3, 40.4, 40.4, 43.2, 43.9, 45.3, 50.2, 52.3, 54.1, 55.3, 56.4, 58.2, 59.8, 60.2, 60.9, 59.3, 57.8, 56.9, 56.1],
+        fill: false,
+        borderColor: 'rgba(75, 192, 192, 1)',
+        tension: 0.1,
+      },
+      {
+        label: 'Shed-west',
+        data: [43.5, 43.9, 44.2, 43.2, 42.4, 41.8, 41.3, 41.4, 41.4, 44.2, 44.9, 46.3, 51.2, 52.3, 53.1, 54.3, 55.4, 57.2, 58.8, 61.2, 61.9, 60.3, 57.8, 56.3, 55.4],
+        fill: false,
+        borderColor: 'rgba(192, 75, 75, 1)',
+        tension: 0.1,
+      },
+    ]
+
+    return temps;
+  },
+
+  fetchLabels: () => {
+    const labels = [
+      '4am', '', '5am', '', '6am', '', '7am', '', '8am', '', '9am', '', '10am', '', '11am', '', 'Noon', '', '1pm', '', '2pm', '', '3pm', '', '4pm'
+    ]
+
+    return labels;
+  }
 }
 
 export default defineComponent({
@@ -44,8 +64,8 @@ export default defineComponent({
       new Chart(ctx, {
         type: 'line',
         data: {
-          labels: ['4am', '', '5am', '', '6am', '', '7am', '', '8am', '', '9am', '', '10am', '', '11am', '', 'Noon', '', '1pm', '', '2pm', '', '3pm', '', '4pm'],
-          datasets: fetchReadings(),
+          labels: TemperatureData.fetchLabels(),
+          datasets: TemperatureData.fetchReadings(),
         },
         options: {
           responsive: true,
