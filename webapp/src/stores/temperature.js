c0866d6946e401f4de35d61a03e44d5e94fa3a3e
darryl west <darryl.west@raincitysoftware.com>
2025-01-29 11:57:40 -0800

    added tempreature store to trigger data update events

---
+++ webapp/src/stores/temperature.js
@@ -0,0 +1,19 @@
+//
+// global temperature data store
+//
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import TemperatureService from '@/services/TemperatureService';
+
+export const useTemperatureStore = defineStore('temperature', () => {
+  const temperatures = ref([]);
+  const labels = ref([]);
+
+  const fetchData = () => {
+    temperatures.value = TemperatureService.fetchReadings();
+    labels.value = TemperatureService.fetchLabels();
+  };
+
+  return { temperatures, labels, fetchData };
+});
+
