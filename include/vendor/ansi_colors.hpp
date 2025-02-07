0f3c2d8fa0582c356b26ef313bf5a1a82094d4bc
darryl west <darryl.west@RainCitySoftware.com>
2025-02-06 20:29:47 -0800

    had to back off the contexpr thing because ubuntu g++ sucks

--- include/vendor/ansi_colors.hpp
+++ include/vendor/ansi_colors.hpp
@@ -14,26 +14,26 @@
 //  https://linux.die.net/man/5/terminfo
 //  https://stackoverflow.com/questions/2616906/how-do-i-output-coloured-text-to-a-linux-terminal
 namespace colors {
-    constexpr std::string red = "\033[0;31m";
-    constexpr std::string green = "\033[0;32m";
-    constexpr std::string yellow = "\033[0;33m";
-    constexpr std::string blue = "\033[0;34m";
-    constexpr std::string magenta = "\033[0;35m";
-    constexpr std::string cyan = "\033[0;36m";
-    constexpr std::string white = "\033[0;37m";
+    std::string red = "\033[0;31m";
+    std::string green = "\033[0;32m";
+    std::string yellow = "\033[0;33m";
+    std::string blue = "\033[0;34m";
+    std::string magenta = "\033[0;35m";
+    std::string cyan = "\033[0;36m";
+    std::string white = "\033[0;37m";
 
     // foreground colors
     namespace bright {
-        constexpr std::string red = "\033[1;31m";
-        constexpr std::string green = "\033[1;32m";
-        constexpr std::string yellow = "\033[1;33m";
-        constexpr std::string blue = "\033[1;34m";
-        constexpr std::string magenta = "\033[1;35m";
-        constexpr std::string cyan = "\033[1;36m";
-        constexpr std::string white = "\033[1;37m";
+        std::string red = "\033[1;31m";
+        std::string green = "\033[1;32m";
+        std::string yellow = "\033[1;33m";
+        std::string blue = "\033[1;34m";
+        std::string magenta = "\033[1;35m";
+        std::string cyan = "\033[1;36m";
+        std::string white = "\033[1;37m";
     }
 
-    constexpr std::string reset = "\033[0m";
+    std::string reset = "\033[0m";
 }
 
 #endif
