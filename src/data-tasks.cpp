79c4b709a1c9f4e6d4dc677e0293b5be4d6906fe
darryl west <darryl.west@raincitysoftware.com>
2025-02-07 08:04:10 -0800

    added data-tasks

---
+++ src/data-tasks.cpp
@@ -0,0 +1,51 @@
+//
+// 2025-02-07 15:47:16 dpw
+//
+
+#include <spdlog/spdlog.h>
+
+#include <app/cli.hpp>
+#include <app/version.hpp>
+#include <vector>
+
+// special
+#include <app/client.hpp>
+#include <app/nodes.hpp>
+#include <thread>
+
+using namespace app::taskrunner;
+
+int main(int argc, char* argv[]) {
+    const auto vers = app::Version();
+
+    auto config = app::parse_cli(argc, argv);
+    spdlog::info("DataTasks Version: {}", vers.to_string());
+
+    // define and start the ticker + jobs
+    // auto tasks = getTasks();
+    // start_tasks(tasks);
+    // list: fetch_temp_readings, fetch_other_readings, fetch_client_status
+    auto nodes = app::client::create_nodes();
+    std::vector<Task> tasks;
+
+    // add all tasks to nodes
+    app::nodes::append_temps_tasks(nodes, tasks);
+    app::nodes::append_status_tasks(nodes, tasks);
+
+    // used for graceful shutdown
+    halt_threads.clear();
+
+    auto tlist = start_tasks(tasks);
+
+    // read a shutdown hook from filesystem or signal
+    //
+    // shut down the ticker tasks
+    //
+    // halt_threads.test_and_set();
+
+    for (auto& t : tlist) {
+        t.join();
+    }
+
+    return 0;
+}
