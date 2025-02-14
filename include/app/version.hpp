ef0610644c2809319ccd0bd4d1e6fa0b8b855b8a
darryl west <darryl.west@raincitysoftware.com>
2025-02-13 20:55:36 -0800

    version bump

--- include/app/version.hpp
+++ include/app/version.hpp
@@ -18,7 +18,7 @@ namespace app {
         int major = 0;
         int minor = 3;
         int patch = 7;
-        int build = 164;
+        int build = 165;
 
         friend std::ostream& operator<<(std::ostream& os, const Version v) {
             // better to use <format> but it breaks on linux and fmt broken on darwin
