d613cc2a9d714374fd2f3d823d20d59630c28e0e
darryl west <darryl.west@raincitysoftware.com>
2025-02-17 09:46:41 -0800

    integrate datetimelib

--- cmake/FindDatetimelib.cmake
+++ cmake/FindDatetimelib.cmake
@@ -10,9 +10,4 @@ if(NOT datetimelib_FOUND)
         GIT_TAG main
     )
     FetchContent_MakeAvailable(datetimelib)
-
-    # Create an alias target to match the expected name
-    if(NOT TARGET datetimelib::datetimelib)
-        add_library(datetimelib::datetimelib ALIAS datetimelib_static)
-    endif()
 endif()
