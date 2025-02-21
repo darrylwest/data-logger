771fca6af2e80ed9f99bbd67aa7b14ac274fb91f
darryl west <darryl.west@raincitysoftware.com>
2025-02-20 18:11:10 -0800

    test config service

--- test/config-service.cpp
+++ test/config-service.cpp
@@ -1,3 +1,8 @@
+//
+// 2025-02-21 01:18:17 dpw
+//
+// this is a pattern for a generic service
+//
 
 #include <string>
 #include <mutex>
@@ -6,115 +11,120 @@
 #include <chrono>
 #include <spdlog/spdlog.h>
 
-struct ServiceConfig {
-    std::string param1 = "default";
-    int param2 = 0;
-    std::chrono::milliseconds sleep_duration{1000}; // Sleep time between work
-};
+namespace app {
+    namespace config {
 
-class MyService {
-public:
-    static MyService& instance() {
-        static MyService service;
-        return service;
-    }
+        struct ServiceConfig {
+            std::string param1 = "default";
+            int param2 = 0;
+            std::chrono::milliseconds sleep_duration{1000}; // Sleep time between work
+        };
 
-    std::string doSomething(const std::string& data) {
-        spdlog::info("do something with this data: {}", data);
-        std::lock_guard<std::mutex> lock(mutex_);
-
-        return data + ">>" + std::to_string(config_.param2);
-    }
-
-    static void configure(const ServiceConfig& config) {
-        spdlog::info("reconfigure the instance");
-        auto& service = instance();
-        std::lock_guard<std::mutex> lock(service.mutex_);
-        service.config_ = config;
-        service.startWorker();
-    }
-
-    // Destructor ensures clean shutdown
-    ~MyService() {
-        spdlog::info("deconstruct: stop the worker");
-        stopWorker();
-    }
-
-private:
-    MyService() = default;
-    MyService(const MyService&) = delete;
-    MyService& operator=(const MyService&) = delete;
-
-    void startWorker() {
-        spdlog::info("start the worker");
-        running_ = true;
-        worker_ = std::thread([this]() { workerLoop(); });
-    }
-
-    void stopWorker() {
-        spdlog::info("stop the worker");
-        running_ = false;
-        if (worker_.joinable()) {
-            worker_.join();
-        }
-    }
-
-    void workerLoop() {
-        int loops = 0;
-        while (running_) {
-            {
-                std::lock_guard<std::mutex> lock(mutex_);
-                loops++;
-                spdlog::info("do the work, loop: {}", loops);
-
-                // change something here...
-                config_.param2++;
+        class MyService {
+        public:
+            static MyService& instance() {
+                static MyService service;
+                return service;
             }
-            
-            std::this_thread::sleep_for(config_.sleep_duration);
+
+            std::string doSomething(const std::string& data) {
+                spdlog::info("do something with this data: {}", data);
+                std::lock_guard<std::mutex> lock(mutex_);
+
+                return data + "-> " + std::to_string(config_.param2);
+            }
+
+            static void configure(const ServiceConfig& config) {
+                spdlog::info("reconfigure the instance");
+                auto& service = instance();
+                std::lock_guard<std::mutex> lock(service.mutex_);
+                service.config_ = config;
+                service.startWorker();
+            }
+
+            // Destructor ensures clean shutdown
+            ~MyService() {
+                spdlog::info("deconstruct: stop the worker");
+                stopWorker();
+            }
+
+        private:
+            MyService() = default; // this ensures it's a singleton
+            MyService(const MyService&) = delete; // prevents copying
+            MyService& operator=(const MyService&) = delete; // prevents re-assignment
+
+            void startWorker() {
+                spdlog::info("start the worker");
+                running_ = true;
+                worker_ = std::thread([this]() { workerLoop(); });
+            }
+
+            void stopWorker() {
+                spdlog::info("stop the worker");
+                running_ = false;
+                if (worker_.joinable()) {
+                    worker_.join();
+                }
+            }
+
+            void workerLoop() {
+                int loops = 0;
+                while (running_) {
+                    {
+                        std::lock_guard<std::mutex> lock(mutex_);
+                        loops++;
+                        spdlog::info("do the work, loop: {}", loops);
+
+                        // change something here...
+                        config_.param2++;
+                    }
+                    
+                    std::this_thread::sleep_for(config_.sleep_duration);
+                }
+            }
+
+            ServiceConfig config_;
+            std::mutex mutex_;
+            std::thread worker_;
+            std::atomic<bool> running_{false};
+        };
+
+        template<typename... Args>
+        std::string do_something(Args&&... args) {
+            return MyService::instance().doSomething(std::forward<Args>(args)...);
         }
-    }
 
-    ServiceConfig config_;
-    std::mutex mutex_;
-    std::thread worker_;
-    std::atomic<bool> running_{false};
-};
-
-namespace myservice {
-    template<typename... Args>
-    std::string do_something(Args&&... args) {
-        return MyService::instance().doSomething(std::forward<Args>(args)...);
-    }
-
-    inline void configure(const ServiceConfig& config) {
-        MyService::configure(config);
+        inline void configure(const ServiceConfig& config) {
+            MyService::configure(config);
+        }
     }
 }
 
 int main() {
+    using namespace app;
+
     // Configure service with custom sleep duration
-    ServiceConfig config;
+    config::ServiceConfig config;
     config.sleep_duration = std::chrono::milliseconds(1500);
-    myservice::configure(config);
+    config::configure(config);
 
     // Service will run in background
-    std::string r = myservice::do_something("hello");
+    std::string r = config::do_something("hello");
     spdlog::info("result: {}", r);
 
     std::this_thread::sleep_for(std::chrono::seconds(2));
-    r = myservice::do_something("hello again");
+    r = config::do_something("hello again");
     spdlog::info("result: {}", r);
 
     std::this_thread::sleep_for(std::chrono::seconds(3));
-    r = myservice::do_something("hello once more");
+    r = config::do_something("hello once more");
     spdlog::info("result: {}", r);
 
     // Your main program continues...
     std::this_thread::sleep_for(std::chrono::seconds(5));
 
     std::this_thread::sleep_for(std::chrono::seconds(3));
-    r = myservice::do_something("over and out");
+    r = config::do_something("over and out");
     spdlog::info("result: {}", r);
 
     std::this_thread::sleep_for(std::chrono::seconds(1));
