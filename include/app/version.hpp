2561aafa1cdfa7ce727990497c664638bbeee280
darryl west <darryl.west@raincitysoftware.com>
2025-02-23 08:02:18 -0800

    version bump and change of database api

--- include/app/version.hpp
+++ include/app/version.hpp
@@ -17,8 +17,8 @@ namespace app {
     struct Version {
         int major = 0;
         int minor = 6;
-        int patch = 3;
-        int build = 192;
+        int patch = 4;
+        int build = 193;
 
         friend std::ostream& operator<<(std::ostream& os, const Version v) {
             // better to use <format> but it breaks on linux and fmt broken on darwin
