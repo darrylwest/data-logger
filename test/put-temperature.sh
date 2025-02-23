ee094ee38ef6d42299ff3ca60490c2a7db33d749
darryl west <darryl.west@raincitysoftware.com>
2025-02-22 18:18:56 -0800

    fixed curl script to use /api

--- test/put-temperature.sh
+++ test/put-temperature.sh
@@ -5,6 +5,6 @@
 
 set -eu
 
-curl -H 'Content-Type: application/json' -XPUT -d '{"key":"1739636749.test-location", "value":10.1234}' http://localhost:9090/temperature
+curl -H 'Content-Type: application/json' -XPUT -d '{"key":"1739636749.test-location", "value":10.1234}' http://localhost:9090/api/temperature
 
 
