6f9e25e02a3e5ff7abd21ec65d0dd11ac4a03f44
darryl west <darryl.west@raincitysoftware.com>
2025-02-26 17:21:59 -0800

    compiles

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,2 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() function
 #include <catch2/catch_all.hpp>  // For Catch2 v3
+
+TEST_CASE("Example test case", "[example]") {
+    REQUIRE(1 + 1 == 2);
+}
