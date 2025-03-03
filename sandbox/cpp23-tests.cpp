0637b180fc3426643712019b2970d32956f2df5b
darryl west <darryl.west@raincitysoftware.com>
2025-03-02 17:04:53 -0800

    print doesn't work, but fmt does

--- sandbox/cpp23-tests.cpp
+++ sandbox/cpp23-tests.cpp
@@ -3,7 +3,8 @@
 //
 
 #include <cstdio>
-#include <print>
+// #include <print> dies on linux
+#include <spdlog/fmt/fmt.h>
 #include <vendor/ansi_colors.hpp>
 
 using namespace colors;
@@ -11,7 +12,7 @@ using namespace colors;
 int main() {
     std::puts("this is a puts console output...");
 
-    std::print("{}formatting {}{:.3f}{}\n", green, yellow, 10.45678, reset);
+    fmt::print("{}formatting {}{:.3f}{}\n", green, yellow, 10.45678, reset);
 
     return 0;
 }
