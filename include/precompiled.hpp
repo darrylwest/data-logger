41e8ddcd449b55b41806d49f962a44ed5447caf8
darryl west <darryl.west@raincitysoftware.com>
2025-03-04 12:23:00 -0800

    attempts to speed up compile times

---
+++ include/precompiled.hpp
@@ -0,0 +1,18 @@
+
+// include/precompiled.hpp
+#ifndef PRECOMPILED_HPP
+#define PRECOMPILED_HPP
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <memory>
+#include <algorithm>
+#include <functional>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+
+#endif // PRECOMPILED_HPP
